09712b723a334b0d1c33ddc72409596235eff1f3
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2013-04-19 10:24:14 +0300

    Public api imports need to be at end of file.

--- morfessor/__init__.py
+++ morfessor/__init__.py
@@ -4,11 +4,6 @@ Morfessor 2.0 - Python implementation of the Morfessor method
 """
 import logging
 
-from .baseline import BaselineModel
-from .cmd import main, get_default_argparser
-from .exception import MorfessorException, ArgumentException
-from .io import MorfessorIO
-from .utils import _progress
 
 __all__ = ['MorfessorException', 'ArgumentException', 'MorfessorIO',
            'BaselineModel', 'main', 'get_default_argparser']
@@ -16,6 +11,7 @@ __all__ = ['MorfessorException', 'ArgumentException', 'MorfessorIO',
 __version__ = '2.0.0alpha3'
 __author__ = 'Sami Virpioja, Peter Smit'
 __author_email__ = "morfessor@cis.hut.fi"
+
 show_progress_bar = True
 
 _logger = logging.getLogger(__name__)
@@ -23,3 +19,13 @@ _logger = logging.getLogger(__name__)
 
 def get_version():
     return __version__
+
+# The public api imports need to be at the end of the file,
+# so that the package global names are available to the modules
+# when they are imported.
+
+from .baseline import BaselineModel
+from .cmd import main, get_default_argparser
+from .exception import MorfessorException, ArgumentException
+from .io import MorfessorIO
+from .utils import _progress

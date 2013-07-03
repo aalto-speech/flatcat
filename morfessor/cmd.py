65b55f976c5935b4bcbda16fa2d32351fff9fd42
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2013-07-03 09:54:00 +0300

    Revert "WIP: bughunting"
    This reverts commit a13e803827488e1e703ddd5a0bdf39335f9e84dd.

--- morfessor/cmd.py
+++ morfessor/cmd.py
@@ -723,6 +723,15 @@ Simple usage examples (training and testing):
             help='Penalty for removal of morphs used in the currently ' +
             'active annotations. ' +
             '(default %(default)s).')
+    add_arg('--annotation-supermorph-penalty', dest='annotationsuperpenalty',
+            type=float, default=0, metavar='<float>',
+            help='Penalty for adding supermorphs of morph bigrams used in '
+            'the currently active annotations. ' +
+            'If such supermorphs are added, ' +
+            'segmentation is likely to prefer them over the submorphs ' +
+            '(because emissions contribute most of the corpus cost). ' +
+            'Set to zero to disable. ' +
+            '(default %(default)s).')
 
     # Options for logging
     add_arg = parser.add_argument_group('logging options').add_argument
@@ -874,8 +883,8 @@ def catmap_main(args):
             analysis_sep=args.analysisseparator)
         model.add_annotations(annotations,
                               args.annotationweight,
-                              multiplier=1,
-                              penalty=-args.annotationpenalty)
+                              -args.annotationpenalty,
+                              -args.annotationsuperpenalty)
 
     if args.develfile is not None:
         develannots = io.read_annotations_file(args.develfile,

9d3a52d48d9009f1014bc18538748d33817636fb
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2013-09-04 10:59:36 +0300

    Controlling number of evaluations is messy.

--- morfessor/optimization.py
+++ morfessor/optimization.py
@@ -34,10 +34,14 @@ class Intervals(object):
     N_INF = object()
     P_INF = object()
 
-    def __init__(self, initial, vector):
+    def __init__(self, initial, vector, bidir):
         self.a = Intervals.P_INF
         self.x = 0.
-        self.b = Intervals.N_INF
+        if bidir:
+            self.b = Intervals.N_INF
+        else:
+            self.b = -0.5
+        self.sign = 1
         self.initial = initial
         self.vector = vector
         self.rejected_prev = False
@@ -72,7 +76,9 @@ class Intervals(object):
         cumulative = 0.
         for (v, c) in zip(self.vector, cues):
             if c is not None:
-                cumulative += v * c
+                print(v, c, self.sign)
+                cumulative += v * c * self.sign
+        print('cumulative {}, rejected_prev {}, sign {}'.format(cumulative, self.rejected_prev, self.sign))
         if cumulative < 0:
             self._flip()
         elif cumulative == 0 and self.rejected_prev:
@@ -80,15 +86,17 @@ class Intervals(object):
     
     def _flip(self):
         (self.a, self.b) = (self.b, self.a)
-        print('flipped. Now ({}, {}, {})'.format(self.b, self.x, self.a))
+        self.sign *= -1
+        print('flipped. Now ({}, {}, {}) sign {}'.format(self.b, self.x, self.a, self.sign))
 
 
 class LineSearchBisection(object):
     def __init__(self, func):
         self.func = func
         
-    def search(self, initial, vector, initial_cues, prev_best_f, evals):
-        intervals = Intervals(initial, vector)
+    def search(self, initial, vector, initial_cues, prev_best_f, evals,
+               bidir=False):
+        intervals = Intervals(initial, vector, bidir)
         intervals.use_direction_cue(initial_cues)
         num_rejections = 0
         best = initial
@@ -97,7 +105,7 @@ class LineSearchBisection(object):
         for _ in range(evals):
             (cursor_x, cursor) = intervals.step()
             (f, cursor_cues) = self.func(cursor, best_f)
-            print('comparing {} > {}'.format(f, best_f))
+            print('at {}. comparing {} > {}'.format(cursor, f, best_f))
             if f > best_f:
                 intervals.accept(cursor_x)
                 best_f = f
@@ -118,10 +126,11 @@ def modified_powells(func, initial, max_iters, max_evals, scale):
         vectors.append([0] * len(initial))
         vectors[i][i] = float(scale)
 
-    evals_left = max_evals
     # + 1 is for the final search along the combination vector
     total_vector_loops = (max_iters * len(vectors)) + 1
-    evals_per_vector = int(math.ceil(float(max_evals) / total_vector_loops))
+    evals_per_vector = int(math.ceil(float(max_evals - 1) / total_vector_loops))
+    print('loops: {}, evals per vec: {}'.format(total_vector_loops, evals_per_vector))
+    evals_left = max_evals - evals_per_vector - 1
     point = initial
     (best_f, cues) = func(initial, None)
     best_vector = 0
@@ -130,12 +139,12 @@ def modified_powells(func, initial, max_iters, max_evals, scale):
     num_rejections = 0
     for iteration in range(max_iters):
         for (vec_num, vector) in enumerate(vectors):
+            print('* doing vector {}: {}. evals_left {}'.format(vec_num, vector, evals_left))
             if evals_left <= 0:
-                # FIXME
-                return point
+                break
             evals = min(evals_left, evals_per_vector)
             (point, f, cues, rej) = line.search(
-                point, vector, cues, best_f, evals)
+                point, vector, cues, best_f, evals, (iteration == 0))
             assert f >= best_f
             if f - best_f > best_vector_increase:
                 best_vector = vec_num
@@ -150,8 +159,11 @@ def modified_powells(func, initial, max_iters, max_evals, scale):
         if point == initial:
             print('No improvement from initial point')
             return initial
+        if evals_left <= 0:
+            break
 
     # finally search along the last combination vector
+    print('* doing final vector: {}'.format(vectors[0]))
     (point, f, cues, rej) = line.search(
         point, vectors[0], cues, best_f, evals_per_vector)
         

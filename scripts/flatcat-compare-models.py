72f5acfdd8813146e5a79fd32b5bf68e1a19aadc
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2015-03-25 15:05:51 +0200

    Started on new comparison tool

---
+++ scripts/flatcat-compare-models.py
@@ -0,0 +1,64 @@
+from __future__ import unicode_literals
+
+import argparse
+import collections
+import sys
+
+import flatcat
+
+LICENSE = """
+Copyright (c) 2014, Stig-Arne Gronroos
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1.  Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+2.  Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following
+    disclaimer in the documentation and/or other materials provided
+    with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
+
+def get_argparser():
+    parser = argparse.ArgumentParser(
+        prog='flatcat-compare-models',
+        description="""
+Morfessor FlatCat model comparison diagnostics
+""",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False)
+    add_arg = parser.add_argument
+    add_arg('modelfile',
+        metavar='<modelfile>',
+        dest='modelfiles',
+        nargs='+')
+    # FIXME: hardcoded to alpha atm
+    #add_arg('-x', '--variable)
+    add_arg('--aligned-reference', dest='alignref', default=None,
+            metavar='<file>',
+            help='FIXME')
+    add_arg('--aligned-to-segment', dest='alignseg', default=None,
+            metavar='<file>',
+            help='FIXME')
+    add_arg('--aligned-loss', dest="alignloss", type=str, default='abs',
+            metavar='<type>', choices=['abs', 'square', 'zeroone', 'tot'],
+            help="loss function for FIXME ('abs', 'square', 'zeroone' or"
+                 "'tot'; default '%(default)s')")
+    return parser
